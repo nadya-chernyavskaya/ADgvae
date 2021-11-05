fixing the bug in kl eval in analyse script

main_analyse_torch.py
@@ -11,7 +11,7 @@ import vande.analysis.analysis_roc as ar
 import pofah.util.experiment as expe
 
 import models_torch.models as models
-import models_torch.losses as losses
+import models_torch.losses as torch_losses
 import utils_torch.scaler
 import utils_torch.preprocessing as prepr
 import utils_torch.plot_util as plot
@@ -64,7 +64,7 @@ with open(os.path.join(experiment.model_dir,'scaler.pkl'), 'rb') as outp:
 RunParameters = namedtuple('RunParameters', saved_params)
 params = RunParameters(**saved_params)
 
-loss_ftn_obj = losses.LossFunction(params.loss_func)
+loss_ftn_obj = torch_losses.LossFunction(params.loss_func)
 model_path = osp.join(experiment.model_dir, params.model_name+'.best.pth')
 model_fname = params.model_name
 model =  getattr(models, params.model_name)(input_dim=params.input_dim,output_dim=params.output_dim, big_dim=params.big_dim, hidden_dim=params.hidden_dim)
@@ -116,7 +116,7 @@ procs_signals = {key: value for key, value in procs_all.items() if value > 0}
 
 save_path = os.path.join(experiment.model_dir, 'predicted_data/')
 pathlib.Path(save_path).mkdir(parents=True, exist_ok=True)
-overwrite = True
+overwrite = False
 #if not osp.isfile(osp.join(save_path,'predicted_df_signal.pkl')) or overwrite:
 if overwrite:
     print('>>> Preparing data')
@@ -161,18 +161,30 @@ else:
         losses+=['loss_tot','loss_kl']
 
     feature_format = 'all_mse' #should be configurable above
+    mu_datas,log_var_data,kl_losses_manual = [],[],[]
     for proc, proc_bit in procs_dict.items(): 
         input_file = pred_sig if proc!='QCD' else pred_qcd
         mask = np.where(np.array(input_file['truth_bit']).astype(int)==proc_bit)[0]
         plot_stat = int(2e4)
         plot.plot_reco(np.array(input_file['input_fts'])[mask][0:plot_stat], np.array(input_file['reco_feats'])[mask][0:plot_stat],scaler, True, model_fname, osp.join(osp.join(fig_dir,'signals'),proc.replace(' ','_')), feature_format,title=proc)
         plot.plot_latent(np.array(input_file['z_0_fts'])[mask][0:plot_stat],np.array(input_file['z_last_fts'])[mask][0:plot_stat],osp.join(osp.join(fig_dir,'signals'),proc.replace(' ','_')),title=proc)
+        #plot mu and sigma
+        mu = np.array(input_file['mu_fts'])[mask][0:plot_stat]
+        log_var = np.array(input_file['log_var_fts'])[mask][0:plot_stat]
+        mu_datas.append(mu)
+        log_var_data.append(log_var)
+        kl_losses_manual.append(torch_losses.kl_loss_manual(mu,log_var))
         #jets
         input_df = df_signal if proc!='QCD' else df_qcd
         for loss in losses:
             input_df['{}_sum'.format(loss)] = input_df['{}_1'.format(loss)]+input_df['{}_2'.format(loss)]
             input_df['{}_min'.format(loss)] = np.minimum(input_df['{}_1'.format(loss)],input_df['{}_2'.format(loss)])
             input_df['{}_max'.format(loss)] = np.maximum(input_df['{}_1'.format(loss)],input_df['{}_2'.format(loss)])
+    vande_plot.plot_features(np.array(mu_datas),'Gauss mu'  ,'Normalized' , '', plotname='{}plot_mu'.format(fig_dir), legend=list(procs_dict.keys()), ylogscale=True)
+    vande_plot.plot_features(np.array(log_var_data), 'Gauss log sigma'  ,'Normalized' , '', plotname='{}plot_log_var'.format(fig_dir), legend=list(procs_dict.keys()), ylogscale=True)
+    vande_plot.plot_hist_many(np.array(kl_losses_manual), 'KL loss manual'  ,'Normalized' , '', plotname='{}plot_kl_loss_manual'.format(fig_dir), legend=list(procs_dict.keys()), ylogscale=True)
+
+
 
     print('Plotting losses')
     for loss in losses:

main_train_torch.py
@@ -47,16 +47,16 @@ num_workers = 0
 # ********************************************************
 RunParameters = namedtuple('Parameters', 'run_n  \
  n_epochs train_total_n valid_total_n proc batch_n learning_rate min_lr patience min_delta adam_betas plotting generator')
-params = RunParameters(run_n=18, 
-                       n_epochs=100, 
+params = RunParameters(run_n=19, 
+                       n_epochs=80, 
                        train_total_n=int(1e6 ),  #2e6 
                        valid_total_n=int(2e5), #1e5
                        proc='QCD_side',
                        batch_n=200, 
-                       learning_rate=0.001,
+                       learning_rate=0.01,
                        min_lr=10e-7,
                        patience=6,
-                       min_delta=0.01,
+                       min_delta=0.005,
                        adam_betas=(0.7,0.9), #0.7, 0.9 #default (0.9, 0.999)
                        plotting=False,
                        generator=1) 
@@ -77,13 +77,13 @@ experiment = expe.Experiment(params.run_n).setup(model_dir=True, fig_dir=True)
 Parameters = namedtuple('Settings', 'model_name  input_dim output_dim loss_func standardizer big_dim hidden_dim beta activation initializer')
 settings = Parameters(model_name = 'PlanarEdgeNetVAE',
                      input_dim=7,
-                     output_dim=7, #3 or 7 
-                     loss_func = 'vae_loss_mse',  #  vae_loss_mse vae_loss_mse_coord',
+                     output_dim=3, #3 or 7 
+                     loss_func = 'vae_loss_mse_coord',  #  vae_loss_mse vae_loss_mse_coord',
                      standardizer=uscaler.BasicStandardizer(),  
                      big_dim=32,
                      hidden_dim=2,
                      beta=0.5,
-                     activation=nn.ReLU(),#nn.LeakyReLU(0.1),
+                     activation=nn.ReLU(),#nn.ReLU(),#nn.LeakyReLU(0.1),
                      initializer='') #not yet set up 
 
 

models/losses.py
@@ -42,6 +42,6 @@ def mse_loss_manual(inputs, outputs):
 
 def kl_loss_manual(z_mean,z_log_var):
     kl = 1. + z_log_var - np.square(z_mean) - np.exp(z_log_var)
-    kl = -0.5 * np.sum(kl, axis=-1)
+    kl = -0.5 * np.mean(kl, axis=-1)
     return np.array(kl)
 

models_torch/losses.py
@@ -13,6 +13,21 @@ multi_gpu = torch.cuda.device_count()>1
 eps = 1e-12
 torch.autograd.set_detect_anomaly(True)
 
+def kl_loss_manual(z_mean,z_log_var):
+    kl = 1. + z_log_var - np.square(z_mean) - np.exp(z_log_var)
+    kl = -0.5 * np.mean(kl, axis=-1)
+    return np.array(kl)
+
+
+def mse_manual(self, y, x):#for some reason convension is : out,in
+    PX_idx, PY_idx, PZ_idx, E_idx, PT_idx, ETA_idx, PHI_idx = range(7)
+    y_phi = math.pi*np.tanh(y[:,PHI_idx])
+    y_eta = 2.5*np.tanh(y[:,ETA_idx])
+    full_y = np.stack((y[:,PX_idx],y[:,PY_idx],y[:,PZ_idx],y[:,E_idx],y[:,PT_idx],y_eta,y_phi), axis=1)
+    mse_loss = np.mean( np.square(x-y), axis=-1)
+    return np.array(mse_loss)
+
+
 
 def xyze_to_ptetaphi_torch(y,log_idx=[]):
     ''' converts an array [N x 100, 4] of particles
@@ -68,16 +83,13 @@ class LossFunction:
     # Reconstruction + KL divergence losses
     def vae_loss_mse_coord(self, y,x, mu, logvar):
         MSE = self.mse_coordinates(y,x)
-        # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-        #KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
         KLD = -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
         return (1-self.beta)*MSE + self.beta*KLD, MSE, KLD
         
+
     # Reconstruction + KL divergence losses
     def vae_loss_mse(self, x, y, mu, logvar):
         MSE = self.mse(y,x)
-        # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-        #KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
         KLD = -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
         return (1-self.beta)*MSE + self.beta*KLD, MSE, KLD
 

utils_torch/analysis_util.py
@@ -121,7 +121,7 @@ def process(data_loader, model, loss_ftn_obj,jet_kin_names):
             truth_bit_per_const = []
             for ib in torch.unique(batch):
                 if  'vae_loss' in loss_ftn_obj.name:
-                    losses_tot[ib],losses_reco[ib],losses_kl[ib] = loss_ftn_obj.loss_ftn(jets_rec[batch==ib], jets_x[batch==ib], mu, log_var)
+                    losses_tot[ib],losses_reco[ib],losses_kl[ib] = loss_ftn_obj.loss_ftn(jets_rec[batch==ib], jets_x[batch==ib], mu[batch==ib], log_var[batch==ib])
                 elif 'mse' in loss_ftn_obj.name:
                     losses_tot[ib] = loss_ftn_obj.loss_ftn(jets_rec[batch==ib], jets_x[batch==ib])
                 #saving truth bit for features